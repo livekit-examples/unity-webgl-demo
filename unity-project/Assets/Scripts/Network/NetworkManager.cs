dcf778b52059e25be8b5a956337b42bd95542102
Théo Monnom <theo.monnom@outlook.com>
2023-01-31 11:45:06 +0100

    Upgrade to client-sdk-unity-web v1.4.0

--- unity-project/Assets/Scripts/Network/NetworkManager.cs
+++ unity-project/Assets/Scripts/Network/NetworkManager.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class NetworkManager : MonoBehaviour
 {
     public static NetworkManager Instance { get; private set; }
-    
+
     public delegate void PacketReceivedDelegate(RemoteParticipant participant, IPacket packet, DataPacketKind kind);
     public event PacketReceivedDelegate PacketReceived;
 
     public delegate void RoomCreatedDelegate(Room room);
     public event RoomCreatedDelegate RoomCreated;
-    
+
     public string ServiceURL = "http://localhost:8080";
     public string LiveKitURL = "ws://localhost:7880";
-    
+
     public Room Room { get; private set; }
     public UnityServiceClient UnityService { get; private set; }
 
@@ -28,10 +28,10 @@ public class NetworkManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
-        
+
         DontDestroyOnLoad(this);
         Instance = this;
-        
+
         UnityService = new UnityServiceClient(this, ServiceURL, 5);
     }
 
@@ -39,11 +39,11 @@ public class NetworkManager : MonoBehaviour
     {
         Room = new Room();
         RoomCreated?.Invoke(Room);
-        
+
         m_PacketReader = new PacketReader();
         m_PacketWriter = new PacketWriter();
         Room.DataReceived += DataReceived;
-        
+
         return Room.Connect(LiveKitURL, token);
     }
 
@@ -54,15 +54,15 @@ public class NetworkManager : MonoBehaviour
             Debug.Log("Received a packet coming from the Server API ? ( Ignoring ..) ");
             return;
         }
-        
+
         var packet = m_PacketReader.UnserializePacket(data);
         if (packet == null)
         {
             Debug.LogError($"Failed to unserialize incoming packet from {participant.Sid}");
             return;
         }
-        
-        PacketReceived?.Invoke(participant, packet, (DataPacketKind) kind);
+
+        PacketReceived?.Invoke(participant, packet, (DataPacketKind)kind);
     }
 
     public JSPromise SendPacket<T>(T packet, DataPacketKind kind, params RemoteParticipant[] participants) where T : IPacket
@@ -70,4 +70,4 @@ public class NetworkManager : MonoBehaviour
         var data = m_PacketWriter.SerializePacket(packet);
         return Room.LocalParticipant.PublishData(data.Array, data.Offset, data.Count, kind, participants);
     }
-} 
+}
